6b602d539006005aefd9f4fc0256ca1a6f8b3e30
super7615 <super7615@naver.com>
2022-09-08 11:38:24 +0900

    [ADD] : LoginActivity init

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/login/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.songforyou.view.login
+
+import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseActivity
+import com.hanyeop.songforyou.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/main/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.songforyou.view.main
+
+import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseActivity
+import com.hanyeop.songforyou.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+}
