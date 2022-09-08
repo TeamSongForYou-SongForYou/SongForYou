0effb2f447af8182e09961f6fbd8f69b71409652
super7615 <super7615@naver.com>
2022-09-08 11:19:27 +0900

    [ADD] : BaseActivity, BaseFragment

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/base/BaseFragment.kt
@@ -0,0 +1,45 @@
+package com.hanyeop.songforyou.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+// 데이터 바인딩을 위한 프래그먼트
+abstract class BaseFragment<T: ViewDataBinding>(
+    @LayoutRes val layoutResId: Int
+) : Fragment(){
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        init()
+    }
+
+    protected fun showToast(msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    protected abstract fun init()
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
