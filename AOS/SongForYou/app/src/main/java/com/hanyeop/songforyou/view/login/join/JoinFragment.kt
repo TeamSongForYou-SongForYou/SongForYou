34c907ccb11e76869f5c0d9a2ca13ea2533f3a75
super7615 <super7615@naver.com>
2022-09-18 14:29:43 +0900

    [ADD] : LoginActivity 패키지 구조 추가

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/login/join/JoinFragment.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.songforyou.view.login.join
+
+import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseFragment
+import com.hanyeop.songforyou.databinding.FragmentJoinBinding
+
+class JoinFragment : BaseFragment<FragmentJoinBinding>(R.layout.fragment_join) {
+    override fun init() {
+
+    }
+}
