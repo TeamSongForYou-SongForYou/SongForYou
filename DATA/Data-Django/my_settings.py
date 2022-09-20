4af0dcfc441205a6742375058bf8ce2d4f99fec9
ChaeMinJi <chaemj97@gmail.com>
2022-09-20 21:25:58 +0900

    [UPDATE] : mysql setting

--- DATA/Data-Django/my_settings.py
+++ DATA/Data-Django/my_settings.py
@@ -2,10 +2,10 @@ DATABASE = {
     'default' : {
         'ENGINE' : 'django.db.backends.mysql',
         'NAME' : 'test',
-        'USER' : 'root',
-        'PASSWORD' :'',
-        'HOST' : 'localhost',
-        'PORT' : '3306'
+        'USER' : 'd207sfy',
+        'PASSWORD' :'d207!@#',
+        'HOST' : 'j7d207.p.ssafy.io',
+        'PORT' : '13306'
     }
 }
 SECRET_KEY = 'django-insecure-pw_-)opcazd3l$0m0$ixwio=m(2j#mh^v2-o7!e=8rtr_$s*o^'
