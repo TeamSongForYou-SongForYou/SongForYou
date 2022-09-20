7dfcfd6f39074dafb21d669108d589c2981163ed
super7615 <super7615@naver.com>
2022-09-20 21:53:48 +0900

    [S07P22D207-122]  ADD : Other 화면 생성

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/main/other/ShowOthersFragment.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.songforyou.view.main.other
+
+import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseFragment
+import com.hanyeop.songforyou.databinding.FragmentShowOthersBinding
+
+class ShowOthersFragment : BaseFragment<FragmentShowOthersBinding>(R.layout.fragment_show_others) {
+    override fun init() {
+        
+    }
+}
