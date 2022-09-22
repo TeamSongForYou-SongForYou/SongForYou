137a6bbfab3b65e07e534708f44889ebedbb6768
super7615 <super7615@naver.com>
2022-09-22 15:42:14 +0900

    ADD : 녹음 기능 구현 (저장X)

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/audio/AudioRecordActivity.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/audio/AudioRecordActivity.kt
@@ -1,11 +1,147 @@
 package com.hanyeop.songforyou.view.audio
 
+import android.Manifest
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.widget.Button
 import com.hanyeop.songforyou.R
 import com.hanyeop.songforyou.base.BaseActivity
 import com.hanyeop.songforyou.databinding.ActivityAudioRecordBinding
+import com.hanyeop.songforyou.utils.State
 
 class AudioRecordActivity : BaseActivity<ActivityAudioRecordBinding>(R.layout.activity_audio_record) {
-    override fun init() {
 
+    private val soundVisualizerView: SoundVisualizerView by lazy {
+        findViewById(R.id.soundVisualizerView)
+    }
+
+    private val recordTimeTextView: CountUpView by lazy {
+        findViewById(R.id.recordTimeTextView)
+    }
+
+    private val resetButton: Button by lazy {
+        findViewById(R.id.resetButton)
+    }
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+    
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+    private var state = State.BEFORE_RECORDING
+        set(value) { // setter 설정
+            field = value // 실제 프로퍼티에 대입
+            resetButton.isEnabled = (value == State.AFTER_RECORDING || value == State.ON_PLAYING)
+            recordButton.updateIconWithState(value)
+        }
+
+    private var recorder: MediaRecorder? = null // 사용 하지 않을 때는 메모리해제 및  null 처리
+    private var player: MediaPlayer? = null
+
+    override fun init() {
+        initViews()
+        bindViews()
+        initVariables()
+    }
+
+    private fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+
+        soundVisualizerView.onRequestCurrentAmplitude = {
+            recorder?.maxAmplitude ?: 0
+        }
+
+
+        recordButton.setOnClickListener {
+            when (state) {
+                State.BEFORE_RECORDING -> {
+                    startRecoding()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+
+        resetButton.setOnClickListener {
+            stopPlaying()
+            // clear
+            soundVisualizerView.clearVisualization()
+            recordTimeTextView.clearCountTime()
+            state = State.BEFORE_RECORDING
+        }
+    }
+
+    private fun initVariables() {
+        state = State.BEFORE_RECORDING
+    }
+
+    private fun startRecoding() {
+        // 녹음 시작 시 초기화
+        recorder = MediaRecorder()
+            .apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP) // 포멧
+                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB) // 엔코더
+                setOutputFile(recordingFilePath) // 우리는 저장 x 캐시에
+                prepare()
+            }
+        recorder?.start()
+        recordTimeTextView.startCountup()
+        soundVisualizerView.startVisualizing(false)
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        soundVisualizerView.stopVisualizing()
+        recordTimeTextView.stopCountup()
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        // MediaPlayer
+        player = MediaPlayer()
+            .apply {
+                setDataSource(recordingFilePath)
+                prepare() // 재생 할 수 있는 상태 (큰 파일 또는 네트워크로 가져올 때는 prepareAsync() )
+            }
+
+        // 전부 재생 했을 때
+        player?.setOnCompletionListener {
+            stopPlaying()
+            state = State.AFTER_RECORDING
+        }
+
+        player?.start() // 재생
+        recordTimeTextView.startCountup()
+
+        soundVisualizerView.startVisualizing(true)
+
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        soundVisualizerView.stopVisualizing()
+        recordTimeTextView.stopCountup()
+
+        state = State.AFTER_RECORDING
     }
 }
---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/audio/SoundVisualizerView.kt
@@ -0,0 +1,128 @@
+package com.hanyeop.songforyou.view.audio
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.hanyeop.songforyou.R
+import kotlin.random.Random
+
+class SoundVisualizerView(
+    context: Context,
+    attributeSet: AttributeSet? = null
+) : View(context, attributeSet) {
+
+    // MainActivity 에서 콜백함수 지정
+    var onRequestCurrentAmplitude: (() -> Int)? = null
+
+    // 계단화 방지 플레그 적용
+    private val amplitudePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = context.getColor(R.color.purple_500)
+        strokeWidth = LINE_WIDTH
+        strokeCap = Paint.Cap.ROUND // 라인의 양 끄투머리 동그랗게
+    }
+
+    private var drawingWidth: Int = 0
+    private var drawingHeight: Int = 0
+    private var drawingAmplitudes: List<Int> = emptyList()
+    private var isReplaying: Boolean = false
+    private var replayingPosition: Int = 0
+
+    // 반복적인 드로우 처리를 위한 Runnable 객체
+    private val visualizeRepeatAction: Runnable = object : Runnable {
+        override fun run() {
+            if (!isReplaying) {
+                // Amplitude를 가져오고, Draw를 요청
+
+                // Amplitude 값 가져오기
+                val currentAmplitude = onRequestCurrentAmplitude?.invoke() ?: 0
+                // 오른 쪽 부터 순차적으로 그리기
+                drawingAmplitudes = listOf(currentAmplitude) + drawingAmplitudes
+            } else {
+                replayingPosition++
+            }
+            invalidate() // 드로잉 처리
+
+            handler?.postDelayed(this, ACTION_INTERVAL)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        drawingWidth = w
+        drawingHeight = h
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        // canvas 가 null 인 경우 반환환
+        canvas ?: return
+
+        val centerY = drawingHeight / 2f // 뷰의 중앙 높이
+        var offsetX = drawingWidth.toFloat() // 오른 쪽 부터
+
+        // 어떤 걸 그릴지
+        // 진폭값을 배열로 넣어두고 오른쪽 부터 왼쪽으로 그려지게
+        drawingAmplitudes
+            .let { amplitudes ->
+                if (isReplaying) {
+                    amplitudes.takeLast(replayingPosition) // 가장 뒤 부터 리플레이 포지션 까지
+                } else {
+                    amplitudes
+                }
+            }
+            .forEach { amplitude ->
+                // 그릴려는 높이 대비 몇퍼로 그릴지 (80%)
+                val lineLength = amplitude / MAX_AMPLITUDE * drawingHeight * 0.8F
+
+                // offset 계산 다시
+                // 뷰의 우측 부터 그릴 것임.
+                offsetX -= LINE_SPACE
+
+                // 진폭 배열이 많이 쌓여 뷰를 초과하는 경우
+                // 보이지 않는 경우 그리지 않아야함
+                if (offsetX < 0) return@forEach
+
+                // amplitude 그리기 (좌상단이 0,0 우하단이 w,h)
+                canvas.drawLine(
+                    offsetX, // 시작
+                    centerY - lineLength / 2F,
+                    offsetX,
+                    centerY + lineLength / 2F,
+                    amplitudePaint
+                )
+            }
+    }
+
+
+    fun startVisualizing(isReplaying: Boolean) {
+        this.isReplaying = isReplaying
+        // 반복 호출 하기 위해 post
+        handler?.post(visualizeRepeatAction)
+    }
+
+    fun stopVisualizing() {
+        // 반복 호출 제거
+        replayingPosition = 0 // 구간 초기화
+        handler?.removeCallbacks(visualizeRepeatAction)
+    }
+
+    fun clearVisualization() {
+        drawingAmplitudes = emptyList()
+        invalidate()
+    }
+
+    // companion object(동반자 객체) = static 비슷 한 개념
+    companion object {
+        private const val LINE_WIDTH = 10F
+        private const val LINE_SPACE = 15F
+
+        // 오디오 레코더의 get max amplitude(진폭, 볼륨) 음성의 최대값의 short 타입 최대값임.
+        private const val MAX_AMPLITUDE = Short.MAX_VALUE.toFloat() // Float로 미리 타입 변환
+
+        private const val ACTION_INTERVAL = 20L // 20밀리초
+    }
+
+}
