d0756d5c671b0fb420138390546cc9ad5e4ed217
super7615 <super7615@naver.com>
2022-09-25 15:51:41 +0900

    [S07P22D207-115]  ADD : UbRecommendRepository

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/repository/UbRecommendRepository.kt
@@ -0,0 +1,30 @@
+package com.hanyeop.songforyou.repository
+
+import com.hanyeop.songforyou.base.BaseResponse
+import com.hanyeop.songforyou.datasource.UbRecommendRemoteDataSource
+import com.hanyeop.songforyou.model.response.RecommendResponse
+import com.hanyeop.songforyou.utils.ResultType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UbRecommendRepository @Inject constructor(
+    private val ubRecommendRemoteDataSource: UbRecommendRemoteDataSource
+) {
+
+    fun getUbRecommendMySound(): Flow<ResultType<BaseResponse<List<RecommendResponse>>>> = flow{
+        emit(ResultType.Loading)
+        ubRecommendRemoteDataSource.getUbRecommendMySound().collect{
+            if(it.success){
+                emit(ResultType.Success(it))
+            }else{
+
+            }
+        }
+    }.catch { e ->
+        emit(ResultType.Error(e))
+    }
+}
