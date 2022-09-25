dbfd5a39bc81ed60f8fb0ea7b84b1f0b1cc84e0d
super7615 <super7615@naver.com>
2022-09-25 16:42:52 +0900

    [S07P22D207-115]  ADD : SbRecommendRemoteDataSource

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/datasource/SbRecommendRemoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.hanyeop.songforyou.datasource
+
+import com.hanyeop.songforyou.api.SbRecommendApi
+import com.hanyeop.songforyou.base.BaseResponse
+import com.hanyeop.songforyou.model.response.RecommendResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.http.Query
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SbRecommendRemoteDataSource @Inject constructor(
+    private val sbRecommendApi: SbRecommendApi
+){
+    fun getSbRecommend(genre: String, age: Int, gender: String, weather: Int): Flow<BaseResponse<List<RecommendResponse>>> = flow{
+        emit(sbRecommendApi.getSbRecommend(genre, age, gender, weather))
+    }
+}
+
