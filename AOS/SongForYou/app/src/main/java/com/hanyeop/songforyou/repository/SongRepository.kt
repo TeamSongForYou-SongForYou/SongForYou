2ed1a1b824b224d74f1d10c4352b5136d5cd77a8
super7615 <super7615@naver.com>
2022-09-25 17:51:22 +0900

    [S07P22D207-115]  ADD : SongRepository

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/repository/SongRepository.kt
@@ -0,0 +1,29 @@
+package com.hanyeop.songforyou.repository
+
+import com.hanyeop.songforyou.base.BaseResponse
+import com.hanyeop.songforyou.datasource.SongRemoteDataSource
+import com.hanyeop.songforyou.model.response.SongDetailResponse
+import com.hanyeop.songforyou.utils.ResultType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SongRepository @Inject constructor(
+    private val songRemoteDataSource: SongRemoteDataSource
+){
+    fun getSongDetail(songSeq: Int): Flow<ResultType<BaseResponse<SongDetailResponse>>> = flow{
+        emit(ResultType.Loading)
+        songRemoteDataSource.getSongDetail(songSeq).collect{
+            if(it.success){
+                emit(ResultType.Success(it))
+            }else if(!it.success){
+                emit(ResultType.Fail(it))
+            }
+        }
+    }.catch { e ->
+        emit(ResultType.Error(e))
+    }
+}
