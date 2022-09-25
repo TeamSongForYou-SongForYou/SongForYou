ceb369eed5094c2305cfc44635cb5073e7ce92df
super7615 <super7615@naver.com>
2022-09-25 16:43:07 +0900

    [S07P22D207-115]  ADD : SbRecommendRepository

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/repository/SbRecommendRepository.kt
@@ -0,0 +1,30 @@
+package com.hanyeop.songforyou.repository
+
+import com.hanyeop.songforyou.base.BaseResponse
+import com.hanyeop.songforyou.datasource.SbRecommendRemoteDataSource
+import com.hanyeop.songforyou.model.response.RecommendResponse
+import com.hanyeop.songforyou.utils.ResultType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SbRecommendRepository @Inject constructor(
+    private val sbRecommendRemoteDataSource: SbRecommendRemoteDataSource
+) {
+
+    fun getSbRecommend(genre: String, age: Int, gender: String, weather: Int): Flow<ResultType<BaseResponse<List<RecommendResponse>>>> = flow{
+        emit(ResultType.Loading)
+        sbRecommendRemoteDataSource.getSbRecommend(genre, age, gender, weather).collect{
+            if(it.success){
+                emit(ResultType.Success(it))
+            }else{
+
+            }
+        }
+    }.catch { e ->
+        emit(ResultType.Error(e))
+    }
+}
