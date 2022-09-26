0ee6193f63c74f009d844799a67ef76c1ba71b59
super7615 <super7615@naver.com>
2022-09-26 13:53:13 +0900

    REFACTOR : RandomFragment => Activity

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/random/RandomRecommendActivity.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.songforyou.view.random
+
+import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseActivity
+import com.hanyeop.songforyou.databinding.ActivityRandomRecommendBinding
+
+class RandomRecommendActivity : BaseActivity<ActivityRandomRecommendBinding>(R.layout.activity_random_recommend) {
+    override fun init() {
+
+    }
+}
