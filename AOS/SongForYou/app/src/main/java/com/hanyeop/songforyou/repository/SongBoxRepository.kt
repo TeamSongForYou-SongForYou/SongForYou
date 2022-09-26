83adc5c7caf0121ac7b51820ba0e20cad3182fef
super7615 <super7615@naver.com>
2022-09-26 13:11:57 +0900

    [S07P22D207-178] ADD : SongBoxRepository

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/repository/SongBoxRepository.kt
@@ -0,0 +1,28 @@
+package com.hanyeop.songforyou.repository
+
+import com.hanyeop.songforyou.base.BaseResponse
+import com.hanyeop.songforyou.datasource.SongBoxRemoteDataSource
+import com.hanyeop.songforyou.utils.ResultType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SongBoxRepository @Inject constructor(
+    private val songBoxRemoteDataSource: SongBoxRemoteDataSource
+){
+    fun addSongBox(songSeq: Int): Flow<ResultType<BaseResponse<String>>> = flow{
+        emit(ResultType.Loading)
+        songBoxRemoteDataSource.addSongBox(songSeq).collect{
+            if(it.success){
+                emit(ResultType.Success(it))
+            }else if(!it.success){
+                emit(ResultType.Fail(it))
+            }
+        }
+    }.catch { e ->
+        emit(ResultType.Error(e))
+    }
+}
