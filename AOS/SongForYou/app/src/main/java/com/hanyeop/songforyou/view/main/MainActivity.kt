0a7add78565f6561845d52193a888d2c53fb67b4
super7615 <super7615@naver.com>
2022-09-26 15:26:51 +0900

    REMOVE : 메인 액티비티 권한 체크 제거

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/main/MainActivity.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/main/MainActivity.kt
@@ -20,8 +20,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     //test
     override fun init() {
         initNavigation()
-
-        initPermission()
     }
 
     private fun initNavigation() {
@@ -32,23 +30,5 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
         // 바텀 네비게이션 연결
         binding.bottomNavigationView.setupWithNavController(navController)
-    }
-
-    private fun initPermission(){
-        TedPermission.create()
-            .setPermissionListener(object : PermissionListener {
-                override fun onPermissionGranted() {
-                }
-                override fun onPermissionDenied(deniedPermissions: List<String>) {
-                    showToast("권한을 허가해주세요.")
-                }
-            })
-            .setDeniedMessage("앱 사용을 위해 권한을 허용으로 설정해주세요. [설정] > [앱 및 알림] > [고급] > [앱 권한]")
-            .setPermissions(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.RECORD_AUDIO
-            )
-            .check()
     }
 }
