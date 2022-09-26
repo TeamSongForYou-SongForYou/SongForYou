1d1c5d8712825dbd3bc0b083aa84da253a0a4f77
super7615 <super7615@naver.com>
2022-09-26 10:35:27 +0900

    [S07P22D207-187] REMOVE: SongResponse 로 합쳐서 SongDetailt 삭제

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/model/response/SongDetailResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.hanyeop.songforyou.model.response
-
-import com.google.gson.annotations.SerializedName
-
-// TODO : Detail 에 뭐가 있는지..?
-data class SongDetailResponse(
-    @SerializedName("song_seq") val SongSeq: Int,
-    )
