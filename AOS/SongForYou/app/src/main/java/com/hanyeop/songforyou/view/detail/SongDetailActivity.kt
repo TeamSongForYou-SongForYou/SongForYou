d171ae185ffe9b7b6e442023fc3b4ff486978b59
super7615 <super7615@naver.com>
2022-09-26 13:53:54 +0900

    REFACTOR : SongDetailFragment => Activity

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/detail/SongDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.songforyou.view.detail
+
+import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseActivity
+import com.hanyeop.songforyou.databinding.ActivitySongDetailBinding
+
+class SongDetailActivity : BaseActivity<ActivitySongDetailBinding>(R.layout.activity_song_detail) {
+    override fun init() {
+
+    }
+}
