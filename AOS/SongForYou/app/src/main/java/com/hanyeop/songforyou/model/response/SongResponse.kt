ac954f5c6ab2b9e8da88fd95d9c1ff800e8fd6ee
super7615 <super7615@naver.com>
2022-09-26 10:34:49 +0900

    [S07P22D207-187]  UPDATE : SongResponse detail 값 수정

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/model/response/SongResponse.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/model/response/SongResponse.kt
@@ -3,10 +3,12 @@ package com.hanyeop.songforyou.model.response
 import com.google.gson.annotations.SerializedName
 
 data class SongResponse (
-    @SerializedName("song_seq") val SongSeq: Int,
-    @SerializedName("song_title") val SongTitle: String,
-    @SerializedName("song_artist_name") val SongArtistName: String,
-    @SerializedName("song_youtube_view") val SongYoutubeView: Int,
-    @SerializedName("song_genre") val SongGenre: Int,
-
+    @SerializedName("songSeq") val SongSeq: Int,
+    @SerializedName("songTitle") val SongTitle: String,
+    @SerializedName("songArtistName") val SongArtistName: String,
+    @SerializedName("songGenre") val SongGenre: Int,
+    @SerializedName("songYoutubeView") val SongYoutubeView: Int,
+    @SerializedName("songThumbnailUrl") val songThumbnailUrl: String,
+    @SerializedName("songYoutubeUrl") val songYoutubeUrl: Int,
+//    @SerializedName("songRegTime") val songRegTime: String,
 )
