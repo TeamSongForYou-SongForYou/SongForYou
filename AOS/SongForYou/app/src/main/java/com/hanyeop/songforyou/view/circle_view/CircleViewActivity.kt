bce32ffe49385a7529f936f9f8e0a850ed526935
super7615 <super7615@naver.com>
2022-09-27 14:35:41 +0900

    STYLE : CenterCircleView 회전

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/circle_view/CircleViewActivity.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/circle_view/CircleViewActivity.kt
@@ -1,17 +1,38 @@
 package com.hanyeop.songforyou.view.circle_view
 
-import android.view.Window
+import androidx.lifecycle.lifecycleScope
 import com.hanyeop.songforyou.R
 import com.hanyeop.songforyou.base.BaseActivity
 import com.hanyeop.songforyou.databinding.ActivityCircleViewBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CircleViewActivity : BaseActivity<ActivityCircleViewBinding>(R.layout.activity_circle_view) {
 
     override fun init() {
         initClickListener()
+
+
     }
 
     private fun initClickListener(){
-
+        binding.apply {
+            btnNext.setOnClickListener{
+                lifecycleScope.launch {
+                    for(i in 0 until 90){
+                        binding.layoutCircle.angleOffset -= 1
+                        delay(3)
+                    }
+                }
+            }
+            btnPrevious.setOnClickListener {
+                lifecycleScope.launch {
+                    for(i in 0 until 90){
+                        binding.layoutCircle.angleOffset += 1
+                        delay(3)
+                    }
+                }
+            }
+        }
     }
 }
