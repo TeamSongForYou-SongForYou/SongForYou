0469fb0d84e89f5aee50b906da487a2393579993
super7615 <super7615@naver.com>
2022-09-29 00:14:08 +0900

    [S07P22D207-173]  ADD : 노래 검색

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/search/SongSearchActivity.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/search/SongSearchActivity.kt
@@ -1,12 +1,51 @@
 package com.hanyeop.songforyou.view.search
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.lifecycleScope
 import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseActivity
+import com.hanyeop.songforyou.databinding.ActivitySongSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-class SongSearchActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_song_search)
+@AndroidEntryPoint
+class SongSearchActivity : BaseActivity<ActivitySongSearchBinding>(R.layout.activity_song_search) {
+
+    private lateinit var songSearchAdapter: SongSearchAdapter
+    private val songSearchViewModel by viewModels<SongSearchViewModel>()
+
+    override fun init() {
+        songSearchAdapter = SongSearchAdapter()
+
+        binding.apply {
+            recyclerSearch.adapter = songSearchAdapter
+        }
+        initViewModelCallBack()
+        
+        initSearchView()
+    }
+    
+    private fun initSearchView(){
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                songSearchViewModel.songSearch(query!!)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun initViewModelCallBack(){
+        lifecycleScope.launch {
+            songSearchViewModel.resultList.collectLatest {
+                songSearchAdapter.submitList(it)
+                binding.tvCount.text = it.size.toString()
+            }
+        }
     }
 }
---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/search/SongSearchAdapter.kt
@@ -0,0 +1,42 @@
+package com.hanyeop.songforyou.view.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hanyeop.songforyou.databinding.ItemRecommendListBinding
+import com.hanyeop.songforyou.model.response.SongResponse
+
+class SongSearchAdapter (): ListAdapter<SongResponse, SongSearchAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val binding: ItemRecommendListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(song: SongResponse) {
+            binding.song = song
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemRecommendListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<SongResponse>() {
+            override fun areItemsTheSame(oldItem: SongResponse, newItem: SongResponse): Boolean {
+                return oldItem.SongSeq == newItem.SongSeq
+            }
+
+            override fun areContentsTheSame(oldItem: SongResponse, newItem: SongResponse): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+        }
+    }
+}
