5f1997ea80737f9b767e1fde80b3d9e0887c93d2
DESKTOP-C5AALBF\enddl <aud0725_123@naver.com>
2022-09-28 17:51:48 +0900

    [S07P22D207-168] UPDATE : 랜덤 추천

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/main/home/HomeViewModel.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/main/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hanyeop.songforyou.model.response.Weather
+import com.hanyeop.songforyou.usecase.sb_recommend.GetSbRecommendRandomUseCase
 import com.hanyeop.songforyou.usecase.weather.GetWeatherUseCase
 import com.hanyeop.songforyou.utils.ResultType
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,14 +14,18 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
+private const val TAG = "HomeViewModel___"
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getWeatherUseCase: GetWeatherUseCase
+    private val getWeatherUseCase: GetWeatherUseCase,
+    private val getSbRecommendRandomUseCase: GetSbRecommendRandomUseCase
 ): ViewModel() {
 
     private val _weatherResponse : MutableStateFlow<ResultType<Weather>> = MutableStateFlow(ResultType.Uninitialized)
     val weatherResponse get() = _weatherResponse.asStateFlow()
+
+    private val _randomList : MutableStateFlow<ResultType<>>
+    val randomList get() = _randomList.asStateFlow()
 
     fun getWeather(dataType : String, numOfRows : Int, pageNo : Int,
                    baseDate : String, baseTime : String, nx : Int, ny : Int){
@@ -28,6 +33,14 @@ class HomeViewModel @Inject constructor(
             getWeatherUseCase.execute(dataType, numOfRows, pageNo, baseDate, baseTime, nx, ny).collectLatest {
                 Log.d("test5", "getWeather: $it")
                 _weatherResponse.value = it
+            }
+        }
+    }
+
+    fun getSbRecommendRandom(){
+        viewModelScope.launch(Dispatchers.IO) {
+            getSbRecommendRandomUseCase.execute().collectLatest {
+                Log.d(TAG, "getSbRecommendRandom$it")
             }
         }
     }
