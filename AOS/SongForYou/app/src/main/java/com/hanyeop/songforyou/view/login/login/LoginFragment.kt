b18828bae1985a6fd6e4fed67e568348432a0621
DESKTOP-KVCQHCD\multicampus <aud0725_123@naver.com>
2022-09-28 11:23:49 +0900

    [[S07P22D207-66] UPDATE : 일반 회원가입

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/login/login/LoginFragment.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/view/login/login/LoginFragment.kt
@@ -1,11 +1,15 @@
 package com.hanyeop.songforyou.view.login.login
 
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.hanyeop.songforyou.R
 import com.hanyeop.songforyou.base.BaseFragment
 import com.hanyeop.songforyou.databinding.FragmentLoginBinding
+import com.hanyeop.songforyou.view.login.UserViewModel
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+
+    private val userViewModel by viewModels<UserViewModel>()
     override fun init() {
         initClickListener()
     }
@@ -16,7 +20,9 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
                 findNavController().navigate(action)
             }
             btnLogin.setOnClickListener{
+                if(userViewModel.email.value.isNullOrBlank() || userViewModel.password.value.isNullOrBlank()){
 
+                }
 
             }
         }
