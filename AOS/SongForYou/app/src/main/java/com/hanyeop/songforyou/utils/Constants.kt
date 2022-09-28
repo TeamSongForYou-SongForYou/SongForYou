4c9d4a4e77b83de53386a81bcf19f67cbef7d914
super7615 <super7615@naver.com>
2022-09-28 11:39:44 +0900

    [S07P22D207-169] ADD : 공공데이터 날씨 호출 주소 추가

--- AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/utils/Constants.kt
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/utils/Constants.kt
@@ -4,6 +4,8 @@ const val BASE_URL = "http://j7d207.p.ssafy.io:8080/"
 
 const val KAKAO_BASE_URL = "https://dapi.kakao.com"
 
+const val WEATHER_BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/"
+
 // JWT
 const val JWT = "JWT-AUTHENTICATION"
 
