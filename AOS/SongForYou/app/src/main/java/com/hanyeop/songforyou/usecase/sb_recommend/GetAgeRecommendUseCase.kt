2cb5502a6a8cf6e90a8b397020f2a775466579d4
DESKTOP-KVCQHCD\multicampus <aud0725_123@naver.com>
2022-09-30 16:38:16 +0900

    [S07P22D207-169]  ADD : 홈화면 리스트 USECASE

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/usecase/sb_recommend/GetAgeRecommendUseCase.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.songforyou.usecase.sb_recommend
+
+import com.hanyeop.songforyou.repository.SbRecommendRepository
+import javax.inject.Inject
+
+class GetAgeRecommendUseCase @Inject constructor(
+    private val sbRecommendRepository: SbRecommendRepository
+){
+//    fun execute( weather: Int)
+//            = sbRecommendRepository.getAgeRecommendUseCase(weather)
+}
