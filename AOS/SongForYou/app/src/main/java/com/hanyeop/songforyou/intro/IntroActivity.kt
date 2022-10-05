42b28167c98f1576c01a95291346aaeacd1083cd
DESKTOP-KVCQHCD\multicampus <aud0725_123@naver.com>
2022-10-05 16:02:56 +0900

    [S07P22D207-169]  STYLE : INTRO 추가

---
+++ AOS/SongForYou/app/src/main/java/com/hanyeop/songforyou/intro/IntroActivity.kt
@@ -0,0 +1,38 @@
+package com.hanyeop.songforyou.intro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.BounceInterpolator
+import androidx.core.view.ViewCompat.animate
+import androidx.databinding.DataBindingUtil.setContentView
+import com.hanyeop.songforyou.R
+import com.hanyeop.songforyou.base.BaseActivity
+import com.hanyeop.songforyou.databinding.ActivityIntroBinding
+import com.hanyeop.songforyou.databinding.ActivityMainBinding
+import com.hanyeop.songforyou.view.login.LoginActivity
+import com.hanyeop.songforyou.view.main.MainActivity
+
+class IntroActivity: BaseActivity<ActivityIntroBinding>(R.layout.activity_intro)  {
+
+    override fun init() {
+        var dp = this.resources.displayMetrics.density
+        binding.ivLogo.animate().translationY(200 * dp).setDuration(1500L)
+            .setInterpolator(BounceInterpolator()).start()
+        var handler = Handler()
+        handler.postDelayed( {
+
+            var intent = Intent( this, LoginActivity::class.java)
+            startActivity(intent)
+        }, 1500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+
+}
